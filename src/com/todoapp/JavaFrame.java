764fb37e8db6f3fe03a7ae6d566810108e24a648
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2022-09-24 14:57:18 +0530

    added comments
    added comments to JavaFrame class

--- src/com/todoapp/JavaFrame.java
+++ src/com/todoapp/JavaFrame.java
@@ -37,22 +37,22 @@ public class JavaFrame extends JFrame implements ActionListener {
         title_panel.setBounds(0, 0, 920, 100); //set Panel on JFrame
         title_panel.setBackground(new Color(177, 178, 255)); //set background color of title_panel
 
-        option_panel = new JPanel();
-        option_panel.setLayout(null);
-        option_panel.setBounds(770, 100, 150, 750);
-        option_panel.setBackground(new Color(237, 240, 255));
+        option_panel = new JPanel(); //creating new JPanel
+        option_panel.setLayout(null); //set layout to null to place manually
+        option_panel.setBounds(770, 100, 150, 750); //placing panel on frame
+        option_panel.setBackground(new Color(237, 240, 255)); //changing background color
 
-        add_button = new JButton(add_logo);
-        add_button.setBounds(20,20,70,70);
-        add_button.addActionListener(this);
+        add_button = new JButton(add_logo); //creating new JButton
+        add_button.setBounds(20,20,70,70); //placing button on option panel
+        add_button.addActionListener(this); //adding action listener to button
 
-        delete_button = new JButton(delete_logo);
-        delete_button.setBounds(20,110,70,70);
-        delete_button.addActionListener(this);
+        delete_button = new JButton(delete_logo); //creating new JButton
+        delete_button.setBounds(20,110,70,70); //placing button on option panel
+        delete_button.addActionListener(this); //adding action listener to button
 
-        badges_button= new JButton(badges_logo);
-        badges_button.setBounds(20,200,70,70);
-        badges_button.addActionListener(this);
+        badges_button= new JButton(badges_logo); //creating new JButton
+        badges_button.setBounds(20,200,70,70); // placing button on option panel
+        badges_button.addActionListener(this); // adding action listener to button
 
         title_label = new JLabel(); //create instance of JLabel
         title_label.setText("Get More Done with Digital Intelligence!"); //set text to JLabel
@@ -60,27 +60,30 @@ public class JavaFrame extends JFrame implements ActionListener {
 
         title_panel.add(title_label); //add title_label to title_panel
 
-        option_panel.add(add_button);
-        option_panel.add(delete_button);
-        option_panel.add(badges_button);
+        option_panel.add(add_button); //adding add button to option panel
+        option_panel.add(delete_button); //adding delete button to option panel
+        option_panel.add(badges_button); //adding badges button to option panel
 
-        this.add(option_panel);
+        this.add(option_panel); //adding option panel to frame
         this.add(title_panel); //adding title_panel to JFrame
 
         this.setResizable(false); //don't allow to expand screen
         this.setVisible(true); //actually display JFrame on device
     }
 
+    //Action listener unimplemented methods
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==add_button)
+        if(e.getSource()==add_button) //if add button is selected
         {
             System.out.println("Button is activated");
         }
-        else if (e.getSource()==delete_button) {
+        else if (e.getSource()==delete_button)  //id delete button is selected
+        {
             System.out.println("Delete Button Activated.");
         }
-        else if(e.getSource()==badges_button){
+        else if(e.getSource()==badges_button) //if badges button is selected
+        {
             System.out.println("Badges button is activated");
         }
     }
